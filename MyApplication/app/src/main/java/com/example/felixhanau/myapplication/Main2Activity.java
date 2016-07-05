08d08a957eca3bfb6a294dfda87a03338e289847
fhanau <fhanau@web.de>
2016-07-05 21:06:48 +0200

    1st commit

---
+++ MyApplication/app/src/main/java/com/example/felixhanau/myapplication/Main2Activity.java
@@ -0,0 +1,34 @@
+package com.example.felixhanau.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        android.webkit.WebView wv = (android.webkit.WebView)findViewById(R.id.wView);
+        //wv.loadUrl("file:///android_asset/myweb.html");
+        wv.loadUrl("file:///android_asset/index1.html");
+        //wv.loadUrl("http://x.org/");
+    }
+
+}
