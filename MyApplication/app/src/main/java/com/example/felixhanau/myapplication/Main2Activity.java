ec5b661708a313ebb73e407a57b4300547593369
fhanau <fhanau@web.de>
2016-09-24 00:12:58 +0200

    RC1

--- MyApplication/app/src/main/java/com/example/felixhanau/myapplication/Main2Activity.java
+++ MyApplication/app/src/main/java/com/example/felixhanau/myapplication/Main2Activity.java
@@ -1,10 +1,9 @@
 package com.example.felixhanau.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.content.Intent;
 import android.view.View;
 import android.webkit.WebView;
 import android.view.View.OnKeyListener;
@@ -15,35 +14,24 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-
-        android.webkit.WebView wv = (android.webkit.WebView)findViewById(R.id.wView);
-
-
+        WebView wv = (WebView)findViewById(R.id.wView);
         wv.setWebViewClient(new android.webkit.WebViewClient() {
             @Override
-            public void onPageFinished(WebView view, String url) {
-                setTitle(view.getTitle());
-            }
-
-            @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if(android.net.Uri.parse(url).getHost().length() == 0) {
+                if(Uri.parse(url).getHost().length() == 0) {
                     return false;
                 }
 
-                android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse(url));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                 view.getContext().startActivity(intent);
                 return true;
             }
         });
         wv.getSettings().setBuiltInZoomControls(true);
-
-
         wv.getSettings().setJavaScriptEnabled(true);
 
+        wv.setOverScrollMode(1);//OVER_SCROLL_IF_CONTENT_SCROLLS
 
         wv.setOnKeyListener(new OnKeyListener()
         {
@@ -54,35 +42,18 @@ public class Main2Activity extends AppCompatActivity {
                 {
                     WebView webView = (WebView) v;
 
-                    switch(keyCode)
+                    if(keyCode == android.view.KeyEvent.KEYCODE_BACK)
                     {
-                        case android.view.KeyEvent.KEYCODE_BACK:
-                            if(webView.canGoBack())
-                            {
-                                webView.goBack();
-                                return true;
-                            }
-                            break;
+                        if(webView.canGoBack())
+                        {
+                            webView.goBack();
+                            return true;
+                        }
                     }
                 }
-
                 return false;
             }
         });
-
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String value = extras.getString("Sitename");
-            String value2 = extras.getString("Filename");
-            setTitle(value);
-            wv.loadUrl(value2);
-        }
-        else {
-            //wv.loadUrl("file:///android_asset/myweb.html");
-            //wv.loadUrl("file:///android_asset/index1.html");
-            wv.loadUrl("file:///android_asset/impressum.html");
-            //wv.loadUrl("http://x.org/");
-        }
+        wv.loadUrl("file:///android_asset/menu.html");
     }
-
 }
